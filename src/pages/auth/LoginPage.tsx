56f33a25f2f663b8982ab33a61c3b6ae18bb40b4
misticslippers <eternal.martyr13579@gmail.com>
2026-01-18 11:15:34 +0300

    add LoginPage.tsx

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,8 +1,10 @@
+import AuthCard from "../../shared/ui/AuthCard";
+import LoginForm from "../../features/auth/ui/LoginForm";
+
 export default function LoginPage() {
     return (
-        <div className="p-6">
-            <h1 className="text-2xl font-bold text-brand-700">Login</h1>
-            <p className="mt-2 text-slate-600">Позже добавим форму.</p>
-        </div>
+        <AuthCard title="Вход">
+            <LoginForm />
+        </AuthCard>
     );
 }
